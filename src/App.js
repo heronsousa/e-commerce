f9ea43f7a52c42357eaaf793303c8ff5202c6cd5
Heron Rodrigues <heronrs1@gmail.com>
2021-07-25 20:51:26 -0300

    Create client form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ name, setName ] = useState('')
+  const [ email, setEmail ] = useState('')
+  const [ cpf, setCpf ] = useState('')
+  const [ phone, setPhone ] = useState('')
+  const [ address, setAddress ] = useState('')
+
+  const handleRegisterClient = (event) => {
+    event.preventDefault();
+
+    const client = {
+      name,email,cpf,phone,address
+    }
+
+    localStorage.setItem("client", JSON.stringify(client));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <form onSubmit={handleRegisterClient}>
+        <h1>Cadastro de cliente</h1>
+        <fieldset>
+          <label>Nome*</label>
+          <input required type="text" placeholder="Insira seu nome" name="nome" autoComplete="name" value={name} onChange={e => {setName(e.target.value)}} />
+        </fieldset>
+        <fieldset>
+          <label>Email*</label>
+          <input required type="email" placeholder="Insira seu email" name="email" autoComplete="email" value={email} onChange={e => {setEmail(e.target.value)}} />
+        </fieldset>
+        <fieldset>
+          <label>CPF*</label>
+          <input required type="text" placeholder="Insira seu cpf" name="cpf" value={cpf} onChange={e => {setCpf(e.target.value)}} />
+        </fieldset>
+        <fieldset>
+          <label>Telefone</label>
+          <input type="phone" placeholder="Insira seu telefone" name="phone" autoComplete="phone" value={phone} onChange={e => {setPhone(e.target.value)}} />
+        </fieldset>
+        <fieldset>
+          <label>Endereço</label>
+          <input type="text" placeholder="Insira seu endereço completo" name="address" autoComplete="address" value={address} onChange={e => {setAddress(e.target.value)}} />
+        </fieldset>
+        
+        <button type="submit">Enviar</button>
+      </form>
+    </main>
   );
 }
 
