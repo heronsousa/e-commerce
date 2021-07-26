84621acb72f791e78cdcbdb0ddfe1e5f60243531
Heron Rodrigues <heronrs1@gmail.com>
2021-07-25 21:11:43 -0300

    Create register product form

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
   const [ cpf, setCpf ] = useState('')
   const [ phone, setPhone ] = useState('')
   const [ address, setAddress ] = useState('')
+  
+  const [ productName, setProductName ] = useState('')
+  const [ productCode, setProductCode ] = useState('')
+  const [ productCount, setProductCount ] = useState(0)
+  const [ productPrice, setProductPrice ] = useState(0)
 
   const handleRegisterClient = (event) => {
     event.preventDefault();
@@ -15,7 +20,25 @@ function App() {
       name,email,cpf,phone,address
     }
 
-    localStorage.setItem("client", JSON.stringify(client));
+    const clients = localStorage.getItem("clients") ? JSON.parse(localStorage.getItem("clients")) : [];
+
+    clients.push(client)
+
+    localStorage.setItem("clients", JSON.stringify(clients));
+  }
+
+  const handleRegisterProduct = (event) => {
+    event.preventDefault();
+
+    const product = {
+      productName, productCount, productCode, productPrice
+    }
+
+    const products = localStorage.getItem("products") ? JSON.parse(localStorage.getItem("products")) : [];
+
+    products.push(product)
+
+    localStorage.setItem("products", JSON.stringify(products));
   }
 
   return (
@@ -44,6 +67,30 @@ function App() {
         </fieldset>
         
         <button type="submit">Enviar</button>
+      </form>
+
+      
+      <form onSubmit={handleRegisterProduct}>
+        <h1>Cadastro de produto</h1>
+        
+        <fieldset>
+          <label>Nome do produto*</label>
+          <input required type="text" placeholder="Insira o nome do produto" name="nome" autoComplete="name" value={productName} onChange={e => {setProductName(e.target.value)}} />
+        </fieldset>
+        <fieldset>
+          <label>Código do produto*</label>
+          <input required type="text" placeholder="Insira o código do produto" name="code" value={productCode} onChange={e => {setProductCode(e.target.value)}} />
+        </fieldset>
+        <fieldset>
+          <label>Quantidade do produto*</label>
+          <input required type="number" placeholder="Insira a quantidade do produto" name="count" value={productCount} onChange={e => {setProductCount(e.target.value)}} />
+        </fieldset>
+        <fieldset>
+          <label>Preço do produto*</label>
+          <input required type="number" placeholder="Insira o preço do produto" name="price" value={productPrice} onChange={e => {setProductPrice(e.target.value)}} />
+        </fieldset>
+        
+        <button type="submit">Cadastrar</button>
       </form>
     </main>
   );
